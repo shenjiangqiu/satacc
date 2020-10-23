7cad5621e393358c1423f2b7a0778533bab1182a
Jiangqiu Shen <jshen2@mtu.edu>
2020-10-23 03:39:23 -0400

    add ideal

--- include/new_intersim_wrapper.h
+++ include/new_intersim_wrapper.h
@@ -24,7 +24,7 @@ private:
     unsigned n_clauses;
 
     void push_into_inct(bool from_cpu_to_mem, unsigned source, unsigned dest, req_ptr req); //tack the ownership
-    unsigned get_pkg_size(bool cpu_to_mem, unsigned source, unsigned dest, const req_ptr &req) const;
+    //unsigned get_pkg_size(bool cpu_to_mem, unsigned source, unsigned dest, const req_ptr &req) const;
 
     std::unordered_set<cache_interface_req *> current_inflight_pkg; //do not own the ownership
 
@@ -70,6 +70,45 @@ protected:
     virtual bool do_cycle() override;
 };
 
+class icnt_ideal : public componet
+{
+    using req_ptr = std::unique_ptr<cache_interface_req>;
+    using req_ptr_q = std::deque<req_ptr>;
+    using req_ptr_q_vec = std::vector<req_ptr_q>;
+    unsigned n_cores;
+    unsigned n_mems;
+    unsigned n_clauses;
+
+public:
+    bool has_pkg_in_icnt() const;
+    bool has_buffer(int level, unsigned source) const;
+    virtual std::string get_internal_size() const override;
+    //virtual double get_busy_percent() const = 0;
+    virtual std::string get_line_trace() const override;
+
+    //interfaces
+    req_ptr_q_vec in_reqs;
+    req_ptr_q_vec out_reqs;
+    req_ptr_q_vec in_resps;
+    req_ptr_q_vec out_resps;
+    //static uint64_t current_cycle;
+    //bool busy;
+
+    virtual bool empty() const;
+
+    icnt_ideal(uint64_t &current_cycle,
+               unsigned num_cores,
+               unsigned num_mems,
+               unsigned num_clauses);
+    //we don't want some global variable to init twice
+    icnt_ideal() = delete;
+    icnt_ideal(const icnt_ideal &) = delete;
+    icnt_ideal(icnt_ideal &&) = delete;
+
+protected:
+    virtual bool do_cycle() override;
+};
+
 class icnt_mesh : public componet
 {
     using req_ptr = std::unique_ptr<cache_interface_req>;
@@ -83,7 +122,7 @@ private:
     unsigned n_clauses;
 
     void push_into_inct(bool from_cpu_to_mem, unsigned source, unsigned dest, req_ptr req); //tack the ownership
-    unsigned get_pkg_size(bool cpu_to_mem, unsigned source, unsigned dest, const req_ptr &req) const;
+    //unsigned get_pkg_size(bool cpu_to_mem, unsigned source, unsigned dest, const req_ptr &req) const;
 
     std::unordered_set<cache_interface_req *> current_inflight_pkg; //do not own the ownership
 
--- src/new_intersim_wrapper.cc
+++ src/new_intersim_wrapper.cc
@@ -8,29 +8,71 @@
 #include <network.h>
 #include <ring.h>
 #include <memreq_info.h>
+unsigned int get_pkg_size(bool cpu_to_mem, unsigned int, unsigned int, const std::unique_ptr<cache_interface_req> &req)
+{
+    auto size = 0;
+    auto type = req->type;
+    //auto as = req->as;
+    switch (type)
+    {
+    case AccessType::ReadClauseData:
+    case AccessType::ReadWatcherData:
+        if (cpu_to_mem)
+        {
+            size = 12;
+        }
+        else
+        {
+            size = 64;
+        }
 
+        /* code */
+        break;
+    case AccessType::ReadClauseValue:
+    case AccessType::ReadWatcherValue:
+        if (cpu_to_mem)
+        {
+            size = 12;
+        }
+        else
+        {
+            size = 10;
+        }
+        /* code */
+        break;
+    case AccessType::writeClause:
+    case AccessType::writeWatcherList:
+        size = 12;
+        break;
+    default:
+        throw std::runtime_error("no such type");
+        break;
+    }
+
+    return size;
+}
 namespace logging = boost::log;
 icnt_ring::icnt_ring(uint64_t &current_cycle,
-                   int num_cores,
-                   int num_mem,
-                   int num_clauses,
-                   int num_vc,
-                   int link_latency,
-                   int arbitration_policy,
-                   int link_width,
-                   int num_vc_cpu) : componet(current_cycle),
-                                     m_ring_network(current_cycle,
-                                                    num_vc, link_latency,
-                                                    arbitration_policy,
-                                                    link_width,
-                                                    num_vc_cpu),
-                                     n_cores(num_cores),
-                                     n_mems(num_mem),
-                                     n_clauses(num_clauses),
-                                     in_reqs(num_cores),
-                                     out_reqs(num_mem),
-                                     in_resps(num_mem),
-                                     out_resps(num_cores)
+                     int num_cores,
+                     int num_mem,
+                     int num_clauses,
+                     int num_vc,
+                     int link_latency,
+                     int arbitration_policy,
+                     int link_width,
+                     int num_vc_cpu) : componet(current_cycle),
+                                       m_ring_network(current_cycle,
+                                                      num_vc, link_latency,
+                                                      arbitration_policy,
+                                                      link_width,
+                                                      num_vc_cpu),
+                                       n_cores(num_cores),
+                                       n_mems(num_mem),
+                                       n_clauses(num_clauses),
+                                       in_reqs(num_cores),
+                                       out_reqs(num_mem),
+                                       in_resps(num_mem),
+                                       out_resps(num_cores)
 {
     //init icnt config
     m_ring_network.init(num_cores, 0, num_mem, 0, 0);
@@ -166,51 +208,6 @@ bool icnt_ring::do_cycle()
     return busy;
 }
 
-unsigned icnt_ring::get_pkg_size(bool cpu_to_mem, unsigned, unsigned, const std::unique_ptr<cache_interface_req> &req) const
-{
-    //push to icnt:
-    auto size = 0;
-    auto type = req->type;
-    //auto as = req->as;
-    switch (type)
-    {
-    case AccessType::ReadClauseData:
-    case AccessType::ReadWatcherData:
-        if (cpu_to_mem)
-        {
-            size = 12;
-        }
-        else
-        {
-            size = 64;
-        }
-
-        /* code */
-        break;
-    case AccessType::ReadClauseValue:
-    case AccessType::ReadWatcherValue:
-        if (cpu_to_mem)
-        {
-            size = 12;
-        }
-        else
-        {
-            size = 10;
-        }
-        /* code */
-        break;
-    case AccessType::writeClause:
-    case AccessType::writeWatcherList:
-        size = 12;
-        break;
-    default:
-        throw std::runtime_error("no such type");
-        break;
-    }
-
-    return size;
-}
-
 bool icnt_ring::has_buffer(int level, unsigned source) const
 {
     return m_ring_network.has_buffer(level, source);
@@ -234,26 +231,26 @@ void icnt_ring::push_into_inct(bool cpu_to_mem, unsigned source, unsigned dest, 
 //TODO make a base class for some function
 
 icnt_mesh::icnt_mesh(uint64_t &current_cycle,
-                   int num_cores,
-                   int num_mem,
-                   int num_clauses,
-                   int num_vc,
-                   int link_latency,
-                   int arbitration_policy,
-                   int link_width,
-                   int num_vc_cpu) : componet(current_cycle),
-                                     m_mesh_network(current_cycle,
-                                                    num_vc, link_latency,
-                                                    arbitration_policy,
-                                                    link_width,
-                                                    num_vc_cpu),
-                                     n_cores(num_cores),
-                                     n_mems(num_mem),
-                                     n_clauses(num_clauses),
-                                     in_reqs(num_cores),
-                                     out_reqs(num_mem),
-                                     in_resps(num_mem),
-                                     out_resps(num_cores)
+                     int num_cores,
+                     int num_mem,
+                     int num_clauses,
+                     int num_vc,
+                     int link_latency,
+                     int arbitration_policy,
+                     int link_width,
+                     int num_vc_cpu) : componet(current_cycle),
+                                       m_mesh_network(current_cycle,
+                                                      num_vc, link_latency,
+                                                      arbitration_policy,
+                                                      link_width,
+                                                      num_vc_cpu),
+                                       n_cores(num_cores),
+                                       n_mems(num_mem),
+                                       n_clauses(num_clauses),
+                                       in_reqs(num_cores),
+                                       out_reqs(num_mem),
+                                       in_resps(num_mem),
+                                       out_resps(num_cores)
 {
     //init icnt config
     m_mesh_network.init(num_cores, 0, num_mem, 0, 0);
@@ -389,51 +386,6 @@ bool icnt_mesh::do_cycle()
     return busy;
 }
 
-unsigned icnt_mesh::get_pkg_size(bool cpu_to_mem, unsigned, unsigned, const std::unique_ptr<cache_interface_req> &req) const
-{
-    //push to icnt:
-    auto size = 0;
-    auto type = req->type;
-    //auto as = req->as;
-    switch (type)
-    {
-    case AccessType::ReadClauseData:
-    case AccessType::ReadWatcherData:
-        if (cpu_to_mem)
-        {
-            size = 12;
-        }
-        else
-        {
-            size = 64;
-        }
-
-        /* code */
-        break;
-    case AccessType::ReadClauseValue:
-    case AccessType::ReadWatcherValue:
-        if (cpu_to_mem)
-        {
-            size = 12;
-        }
-        else
-        {
-            size = 10;
-        }
-        /* code */
-        break;
-    case AccessType::writeClause:
-    case AccessType::writeWatcherList:
-        size = 12;
-        break;
-    default:
-        throw std::runtime_error("no such type");
-        break;
-    }
-
-    return size;
-}
-
 bool icnt_mesh::has_buffer(int level, unsigned source) const
 {
     return m_mesh_network.has_buffer(level, source);
@@ -450,4 +402,94 @@ void icnt_mesh::push_into_inct(bool cpu_to_mem, unsigned source, unsigned dest, 
     current_inflight_pkg.insert(req.get());
     //icnt_push(source, dest, (void *)req.release(), size); //give the ownership
     m_mesh_network.send(req.release(), srclevel, source, dstlevel, dest);
+}
+
+bool icnt_ideal::has_pkg_in_icnt() const
+{
+    return empty();
+}
+bool icnt_ideal::has_buffer(int level, unsigned int source) const
+{
+    return level == 0 ? in_reqs[source].size() <= 16 : in_resps[source].size() <= 16;
+}
+std::string icnt_ideal::get_internal_size() const
+{
+
+    std::string ret;
+    for (auto i = 0u; i < n_cores; i++)
+    {
+        ret += fmt::format("name id in_req out_req \n{}-{} {} {}\n",
+                           "icnt", i,
+                           in_reqs[i].size(),
+                           out_resps[i].size());
+    }
+    for (auto i = 0u; i < n_mems; i++)
+    {
+        ret += fmt::format("name id in_resp out_resp \n{}-{} {} {}\n",
+                           "icnt", i,
+                           in_resps[i].size(),
+                           out_reqs[i].size());
+    }
+    return ret;
+}
+std::string icnt_ideal::get_line_trace() const
+{
+
+    return fmt::format("name busy_percent\n{} {}", "icnt", get_busy_percent());
+}
+bool icnt_ideal::empty() const
+{
+    return std::all_of(in_reqs.begin(), in_reqs.end(), [](auto &q) { return q.empty(); }) and
+           std::all_of(out_reqs.begin(), out_reqs.end(), [](auto &q) { return q.empty(); }) and
+           std::all_of(in_resps.begin(), in_resps.end(), [](auto &q) { return q.empty(); }) and
+           std::all_of(out_resps.begin(), out_resps.end(), [](auto &q) { return q.empty(); });
+}
+icnt_ideal::icnt_ideal(uint64_t &current_cycle, unsigned num_cores, unsigned num_mems, unsigned num_clauses)
+    : componet(current_cycle), n_cores(num_cores), n_mems(num_mems), n_clauses(num_clauses) {}
+
+bool icnt_ideal::do_cycle()
+{
+    for (auto &&[i, q] : enumerate(in_reqs))
+    {
+        if (!q.empty())
+        {
+            busy = true;
+            auto &req = q.front();
+            assert(req);
+            //q.pop_front();
+
+            auto size = get_pkg_size(true, i, n_cores + 1, req);
+            req->m_size = size;
+            auto mem_partition_id = get_partition_id_by_addr(get_addr_by_req(req), n_mems);
+            out_reqs[mem_partition_id].push_back(std::move(req));
+            q.pop_front();
+        }
+    }
+
+    for (auto &&[i, q] : enumerate(in_resps))
+    {
+        //from mem to cores
+
+        if (!q.empty())
+        {
+            auto &req = q.front();
+            assert(i == get_partition_id_by_addr(get_addr_by_req(req), n_mems));
+            auto size = get_pkg_size(false, i, 0, req);
+            req->m_size = size;
+            busy = true;
+            auto dest = req->ComponentId;
+            //fix bug here, this is >= not >!!!
+            if (dest >= n_cores)
+            {
+                dest = dest - n_cores;
+                auto c_to_w = n_clauses / n_cores;
+                dest = dest / c_to_w;
+            }
+            //auto source = resp + n_cores;
+            out_resps[dest].push_back(std::move(req));
+
+            q.pop_front();
+        }
+    }
+    return busy;
 }
